192dd5ded9c1e5d170acc4ffc5b39705e7c8e433
derek_lee <20820343@student.curtin.edu.au>
2022-11-11 18:12:50 +0400

    feat: Added example for overriding highlights misc: renamed themes.lua

--- custom/overidden_themes.lua
+++ custom/overidden_themes.lua
@@ -1,8 +1,9 @@
-local M
-
 -- Follow same format for other themes and highlight groups
 -- For base_16 and base_30 variables: https://github.com/NvChad/base46
 -- Color can also be NONE for transparent or variable names (blue, white, etc)
+
+local M
+
 M = {
   tokyonight = {
     base_30 = {
