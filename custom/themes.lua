6612ee54d21b6d1a7bcb5123f7198c94f24b86a3
derek_lee <20820343@student.curtin.edu.au>
2022-11-11 17:47:04 +0400

    misc: removed darker_black as variable name in example

--- custom/themes.lua
+++ custom/themes.lua
@@ -2,7 +2,7 @@ local M
 
 -- Follow same format for other themes and highlight groups
 -- For base_16 and base_30 variables: https://github.com/NvChad/base46
--- Color can also be NONE for transparent or variable names (blue, white, darker_black, etc)
+-- Color can also be NONE for transparent or variable names (blue, white, etc)
 M = {
   tokyonight = {
     base_30 = {
