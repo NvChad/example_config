90114138812b3acd86dfed17b87b24fd38bf32cd
lucario387 <hoangtun0810@gmail.com>
2022-12-25 10:28:00 +0900

    Add comments for navigation in insert mode (not enabled by default)

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,16 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
   },
+  i = {
+    -- If your terminal supports input disambiguation 
+    -- (i.e. Ctrl-I with Tab, Ctrl-H with Backspace)
+    -- Then you can consider uncommenting these lines to 
+    -- navigate within insert mode
+    -- ["<C-h>"] = { "<Left>", "move left" },
+    -- ["<C-l>"] = { "<Right>", "move right" },
+    -- ["<C-j>"] = { "<Down>", "move down" },
+    -- ["<C-k>"] = { "<Up>", "move up" },
+  }
 }
 
 -- more keybinds!
